1b21a7f3c3a14ce14ef39ae53bcc82cd588397c7
name-heshun <2586625136@qq.com>
2018-07-19 10:25:33 +0800

    新建

---
+++ src/main/java/name/heshun/trigram/TrigramApplication.java
@@ -0,0 +1,12 @@
+package name.heshun.trigram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrigramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrigramApplication.class, args);
+	}
+}
